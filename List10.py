ccf2f8dd41453b4988ae040779d6cc93e4db386e
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-26 17:29:04 +0500

    10

--- List10.py
+++ List10.py
@@ -6,4 +6,8 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return
+    if list_num[0]>list_num[-1]:
+        return list_num[0]
+    else:
+        return list_num[-1]
+print(main([7,2,3,4,5]))
