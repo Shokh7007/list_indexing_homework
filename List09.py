e4cc57a4780961c945678a44927fa7ad40326f74
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-28 21:01:20 +0500

    9

--- List09.py
+++ List09.py
@@ -6,4 +6,11 @@ def main(list1):
     Returns:
         bool: return answer
     """
-    return
+    a=0
+    isTrue=True
+    while a<len(list1)-1:
+        if list1[a]!=list1[a+1]:
+            isTrue=False
+        a+=1
+    return isTrue
+print(main(['asdf','asdf','asdf',123]))
